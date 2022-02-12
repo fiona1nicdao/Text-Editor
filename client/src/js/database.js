bc982bb04aaf4e1d555fa9f2ecfbd5a4d6b094a9
FionaN <fiona.nicdao@northwestern.edu>
2022-02-12 17:08:04 -0600

    update databased.js and install.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,12 +14,33 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
+// Method that takes some content and adds it to the IndexDB database using the i db module
 export const putDb = async (content) => {
-  console.log('PUT', 'to the database')
+  console.log('PUT to the database');
+  const jateDb = await openDB('jate,1');
+  const tx = jateDb.transaction('jate','readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({id:1, value:content});
+  const result = await request;
+  console.log(':rocket: - data saved to the database', result.value);
 }
 // console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Method that gets content from the IndexDB database using the idb module 
+export const getDb = async () => {
+  console.log('GET from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate','readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  result 
+    ? console.log(':rocket: -data retrieved from the databased', result.value)
+    : console.log(':rocket: -data not found in the database');
+  // Check if a varible is defined and if it is, return it. (Optional Chaining)
+  return result?.value;
+};
+//  console.error('getDb not implemented');
 
 initdb();
